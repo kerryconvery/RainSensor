a430c700835e2b63e0d95d384064b8f52fab97f2
DESKTOP-UE92379\Kerry <kerry.convery@myob.com>
2022-10-27 20:57:49 +1100

    reduce rain detector power consumption

--- RainSensor.ino
+++ RainSensor.ino
@@ -1,9 +1,14 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <HTTPClient.h>
-#include <BlynkSimpleEsp32.h>
+#include "driver/adc.h"
+#include <esp_wifi.h>
+#include <esp_bt.h>
 
 #define SENSOR_PIN 2
+#define WIFI_TIMEOUT 10000 // 10seconds in milliseconds
+#define RAINING GPIO_INTR_LOW_LEVEL
+#define SUNNY GPIO_INTR_HIGH_LEVEL
 
 // Initialize Wifi connection to the router
 const char* ssid = "BelongA5465A";     // your network SSID (name)
@@ -11,60 +16,59 @@ const char* password = "nvxjh9nrypzd"; // your network key
 String iftttEventPath = "http://maker.ifttt.com/trigger/rain_detected/with/key/dSYBSCF4exP6Bk8vmw8ChJ81OM7mw4elKfuSBHjFfrx";
 char serverAddress[] = "http://maker.ifttt.com";
 bool notificationSent = false;
-
-/*WiFiClientSecure client;*/
 WiFiClient wifi;
 
 void setup() {
-
   Serial.begin(115200);
   pinMode(SENSOR_PIN, INPUT);
-  // Set WiFi to station mode and disconnect from an AP if it was Previously
-  // connected
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-
-  // Attempt to connect to Wifi network:
-  Serial.print("Connecting Wifi: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
+  
   notificationSent = false;
+  setCpuFrequencyMhz(80);
+  
+  sleepUntil(RAINING);
 }
 
 void loop() {
-  bool isRaining = digitalRead(SENSOR_PIN) == 0;
+  delay(2000);
   
-  Serial.print("Is raining ");
-  Serial.println(isRaining);
-
-  if (!isRaining) {
-    notificationSent = false;
+  Serial.println("Woke up!");  
+  
+  bool isRaining = getIsRaining();
+  
+  if(isRaining) {
+    Serial.println("It is raining");
+  } else {
+    Serial.println("It is sunny");
   }
 
-  if (isRaining && !notificationSent) {
+  if (isRaining) {
     sendRainNotification();
-    notificationSent = true;
-  }
+    
+    Serial.println("Sleep until it is sunny");
+    
+    sleepUntil(SUNNY);
+  } else {
+    Serial.println("Sleep until it is raining");
 
-  delay(1000);  
+    sleepUntil(RAINING);    
+  }
 }
 
-bool sendRainNotification() {
+bool getIsRaining()
+{
+  return digitalRead(SENSOR_PIN) == 0;
+}
+
+void sendRainNotification() {
   int err = 0;
   int responseCode = 0;
   HTTPClient http;
+
+  connectToWiFi();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    return;    
+  }
 
   http.begin(iftttEventPath.c_str());
 
@@ -74,12 +78,54 @@ bool sendRainNotification() {
 
   if (responseCode <= 0) {
     Serial.print("Responded with error code ");
-    Serial.println(responseCode);
-    return false;        
+    Serial.println(responseCode);      
   }
 
   Serial.print("Responded with http code ");
   Serial.println(responseCode);
-    
-  return true;  
+}
+
+void connectToWiFi()
+{
+  Serial.print("Connecting to WiFi... ");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+	// Keep track of when we started our attempt to get a WiFi connection
+  unsigned long startAttemptTime = millis();
+
+  // Keep looping while we're not connected AND haven't reached the timeout
+  while (WiFi.status() != WL_CONNECTED && 
+          millis() - startAttemptTime < WIFI_TIMEOUT){
+    delay(10);
+  }
+
+  // Make sure that we're actually connected, otherwise go to deep sleep
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("FAILED");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void sleepUntil(gpio_int_type_t wakeOnLevel)
+{
+  Serial.println("Going to sleep...");
+  Serial.flush();
+
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  btStop();
+
+  adc_power_off();
+  esp_wifi_stop();
+  esp_bt_controller_disable();
+  
+  gpio_wakeup_enable(GPIO_NUM_2, wakeOnLevel);
+  esp_sleep_enable_gpio_wakeup();
+  
+  esp_light_sleep_start(); 
 }
